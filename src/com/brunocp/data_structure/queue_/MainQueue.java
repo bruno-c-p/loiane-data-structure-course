ae0233084c784996a072c9c2471f6a1e3d221fbc
Bruno Cardozo <bruno.cardozo.pereira@gmail.com>
2022-07-21 20:20:32 -0300

    Implementing own queue class

---
+++ src/com/brunocp/data_structure/queue_/MainQueue.java
@@ -0,0 +1,11 @@
+package com.brunocp.data_structure.queue_;
+
+public class MainQueue {
+
+    public static void main(String[] args) {
+
+        Queuee<Integer> queue = new Queuee<>();
+
+        
+    }
+}
---
+++ src/com/brunocp/data_structure/queue_/Queuee.java
@@ -0,0 +1,14 @@
+package com.brunocp.data_structure.queue_;
+
+import com.brunocp.data_structure.stack_.StaticStructure;
+
+public class Queuee<T> extends StaticStructure<T> {
+
+    public Queuee() {
+        super();
+    }
+
+    public Queuee(int capacity) {
+        super(capacity);
+    }
+}
